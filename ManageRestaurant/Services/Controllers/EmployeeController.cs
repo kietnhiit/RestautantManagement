9457bd8a6b6e7b82bcb1b6819a0d21b68150e74d
nguyendachongan <520annguyen@gmail.com>
2017-08-25 09:06:35 +0700

    api

---
+++ ManageRestaurant/Services/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DAL;
+using Services.Models;
+namespace Services.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        EmployeeDAL eDAL;
+        public EmployeeController()
+        {
+            eDAL = new EmployeeDAL();
+        }
+        public IHttpActionResult GetAllEmployee()
+        {
+            var lst = eDAL.GetAllEmployee();
+            if (lst.Count == 0) return NotFound();
+            return Ok(lst);
+        }
+        
+    }
+}
---
+++ ManageRestaurant/Services/Models/EmployeeViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Services.Models
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+
+        public string Password { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public bool Gender { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        public int TypeID { get; set; }
+
+        public string TypeName { get; set; }
+    }
+}
